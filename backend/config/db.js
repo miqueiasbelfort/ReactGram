fca3c4b6cbcaf45204a178a66f8aedd796145e91
miqueiasbelfort <kawasousa1554@gmail.com>
2022-06-06 16:29:40 -0300

    db conn

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,3 +1,17 @@
 const mongoose = require("mongoose")
+const dbUser = ''
+const dpPassword = ''
 
 //connection
+const conn = async () => {
+    try {
+
+        const dbConn = await mongoose.connection(
+            'mongodb+srv://Miqueias:<password>@reactgram.ce32l2b.mongodb.net/?retryWrites=true&w=majority'
+        )
+        console.log("DB connected")
+
+    } catch(error) {
+
+    }
+}
