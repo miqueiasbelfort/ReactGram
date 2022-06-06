ac8ee1fb5b1c72b2be3285e6471bb8874d1fc4b4
miqueiasbelfort <kawasousa1554@gmail.com>
2022-06-06 19:54:09 -0300

    connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,17 +1,24 @@
+require("dotenv").config()
 const mongoose = require("mongoose")
-const dbUser = ''
-const dpPassword = ''
+
+const dbUser = process.env.DB_USER
+const dbPassword = process.env.DB_PASS
 
 //connection
 const conn = async () => {
     try {
 
-        const dbConn = await mongoose.connection(
-            'mongodb+srv://Miqueias:<password>@reactgram.ce32l2b.mongodb.net/?retryWrites=true&w=majority'
+        const dbConn = await mongoose.connect(
+            `mongodb+srv://${dbUser}:${dbPassword}@reactgram.ce32l2b.mongodb.net/?retryWrites=true&w=majority`
         )
         console.log("DB connected")
 
-    } catch(error) {
+        return dbConn
 
+    } catch(error) {
+        console.log(error)
     }
 }
+
+conn()
+module.exports = conn
