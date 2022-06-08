eba466ae9b5466ba8340c57e17c7c5dc1bcbb3cd
miqueiasbelfort <kawasousa1554@gmail.com>
2022-06-08 09:06:11 -0300

    register user

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,20 +1,50 @@
 require("dotenv").config()
 const User = require("../models/User")
-const bcrypt = require("bcryptjs")
+const bcrypt = require("bcrypt")
 const jwt = require("jsonwebtoken")
 
 const secretJwt = process.env.JWT_SCRET
 
 // generate user token
 const generateToken = (id) => {
-    return jwt.sing({id}, secretJwt, {
+    return jwt.sign({id}, secretJwt, {
         expiresIn: "7d"
     })
 }
 
 // Register use an sign in
 const register = async (req, res) => {
-    res.send("register")
+    const {name, email, password} = req.body
+
+    //check if user exist
+    const user = await User.findOne({email})
+    if(user){
+        res.status(422).json({error: ["E-mail já cadastrado!"]})
+        return
+    }
+
+    // create hash
+    const salt = await bcrypt.genSalt(10)
+    const passowrdHash = await bcrypt.hash(password, salt)
+
+    // create user
+    const newUser = await User.create({
+        name, 
+        email,
+        password: passowrdHash
+    })
+
+    // if user was create successfully
+    if(!newUser){
+        res.status(422).json({errors: ["Houve um erro, por favor tente mais tarde."]})
+        return
+    }
+
+    res.status(201).json({
+        _id: newUser._id,
+        token: generateToken(newUser._id)
+    })
+
 }
 
 module.exports = {
