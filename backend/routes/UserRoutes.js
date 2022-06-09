6bc526f095c901958775da9d8dfad08b1212898f
miqueiasbelfort <kawasousa1554@gmail.com>
2022-06-09 15:21:39 -0300

    profile router

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -1,14 +1,16 @@
 const router = require("express").Router()
 
 //controller
-const {register, login} = require('../controllers/UserController')
+const {register, login, getCurrentUser} = require('../controllers/UserController')
 
 //middlewares
 const validate = require("../middlewares/handleValidation")
 const {userCreateValidation, loginValidation} = require("../middlewares/userValidation")
+const authGuard = require("../middlewares/authGuard")
 
 //routes
 router.post("/register", userCreateValidation(), validate, register)
 router.post("/login", loginValidation(), validate, login)
+router.get("/profile", authGuard, getCurrentUser)
 
 module.exports = router
