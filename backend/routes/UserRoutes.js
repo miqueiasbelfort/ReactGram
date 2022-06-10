e24dbb69068b9d568995010d46e6b3a51a69a104
miqueiasbelfort <kawasousa1554@gmail.com>
2022-06-10 14:37:30 -0300

    getUserById

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router()
 
 //controller
-const {register, login, getCurrentUser, update} = require('../controllers/UserController')
+const {register, login, getCurrentUser, update, getUserById} = require('../controllers/UserController')
 
 //middlewares
 const validate = require("../middlewares/handleValidation")
@@ -14,5 +14,7 @@ router.post("/register", userCreateValidation(), validate, register)
 router.post("/login", loginValidation(), validate, login)
 router.get("/profile", authGuard, getCurrentUser)
 router.put("/", authGuard, userUpdateValidation(), validate, ImageUpload.single("profileImage"), update)
+router.get("/:id", getUserById)
+
 
 module.exports = router
