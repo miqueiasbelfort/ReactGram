7f6a4b692edf715ba053fbc7a50a5603c56b859f
miqueiasbelfort <kawasousa1554@gmail.com>
2022-06-13 15:12:40 -0300

    get photo by ID

--- backend/controllers/PhotoController.js
+++ backend/controllers/PhotoController.js
@@ -82,9 +82,24 @@ const getUserPhotos = async (req, res) => {
  
 }
 
+//Get photo by ID
+const getPhotoById = async (req, res) => {
+    const {id} = req.params
+    const photo = await Photo.findById(mongoose.Types.ObjectId(id))
+
+    //check if photo exists
+    if(!photo){
+        res.status(404).json({errors: ["Foto não encontrada."]})
+        return
+    }
+
+    res.status(200).json(photo)
+}
+
 module.exports = {
     insertPhoto,
     deletePhoto,
     getAllPhotos,
-    getUserPhotos
+    getUserPhotos,
+    getPhotoById
 }
--- backend/routes/PhotoRoutes.js
+++ backend/routes/PhotoRoutes.js
@@ -1,7 +1,13 @@
 const router = require("express").Router()
 
 //controller
-const {insertPhoto, deletePhoto, getAllPhotos, getUserPhotos} = require("../controllers/PhotoController")
+const {
+    insertPhoto, 
+    deletePhoto, 
+    getAllPhotos, 
+    getUserPhotos,
+    getPhotoById
+} = require("../controllers/PhotoController")
 
 // middlewares
 const {photoInsertValidation} = require("../middlewares/photoValidation")
@@ -14,6 +20,7 @@ router.post("/", authGuard, ImageUpload.single("image"), photoInsertValidation()
 router.delete("/:id", authGuard, deletePhoto)
 router.get("/", authGuard, getAllPhotos)
 router.get("/user/:id", authGuard, getUserPhotos)
+router.get("/:id", authGuard, getPhotoById)
 
 
 module.exports = router
