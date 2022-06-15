314b2248a61a38c89496c36c425bd19bee1fff64
miqueiasbelfort <kawasousa1554@gmail.com>
2022-06-15 09:43:08 -0300

    navbar & footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,11 @@ import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom"
 
 import './App.css'
 
+//components
+import Navbar from './components/Navbar'
+import Footer from "./components/Footer"
+
+//pages
 import Home from './pages/Home/Home'
 import Login from './pages/Auth/Login'
 import Register from './pages/Auth/Register'
@@ -12,14 +17,19 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      
-        <Routes>
+        <Navbar />
+
+        <div className='container'>
+          <Routes>
 
           <Route path="/" element={<Home/>}/>
           <Route path="/login" element={<Login/>}/>
           <Route path="/register" element={<Register/>}/>
 
-        </Routes>
+          </Routes>
+        </div>
+        
+        <Footer/>
 
       </BrowserRouter>
     </div>
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import "./Footer.css"
+
+const Footer = () => {
+  return (
+    <footer id="footer">
+      <p>ReactGram &copy;</p>
+    </footer>
+  )
+}
+
+export default Footer
