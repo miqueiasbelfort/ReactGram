b943e0d2c1ba3846d3e08cd116b75be459544069
miqueiasbelfort <kawasousa1554@gmail.com>
2022-06-15 10:11:44 -0300

    register form

--- frontend/src/pages/Auth/Register.jsx
+++ frontend/src/pages/Auth/Register.jsx
@@ -1,9 +1,43 @@
 import "./Auth.css"
 
+//components
+import {Link} from "react-router-dom"
+
+//hooks
+import { useState, useEffect } from "react"
+
 const Register = () => {
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
   return (
-    <div>
-Register
+    <div id="register">
+      <h2>ReactGram</h2>
+      <p className="subtitle">Veja as fotos dos seus amigos</p>
+      <form onSubmit={handleSubmit}>
+        <input 
+          type="text"
+          placeholder="Nome" 
+        />
+        <input 
+          type="email"
+          placeholder="E-mail"
+        />
+        <input 
+          type="password"
+          placeholder="Senha" 
+        />
+        <input 
+          type="password"
+          placeholder="Confirme a senha" 
+        />
+        <input type="submit" value="Cadastrar"/>
+      </form>
+      <p>
+        Já tem conta? <Link to="/login">Clique aqui.</Link>
+      </p>
     </div>
   )
 }
