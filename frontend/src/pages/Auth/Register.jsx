78185fac942fc51fa15775c145779334d5986ac3
miqueiasbelfort <kawasousa1554@gmail.com>
2022-06-16 09:44:26 -0300

    utils

--- frontend/src/pages/Auth/Register.jsx
+++ frontend/src/pages/Auth/Register.jsx
@@ -7,9 +7,22 @@ import {Link} from "react-router-dom"
 import { useState, useEffect } from "react"
 
 const Register = () => {
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [confirmPassword, setConfirmPassword] = useState("")
 
   const handleSubmit = (e) => {
     e.preventDefault()
+  
+    const user = {
+      name,
+      email,
+      password,
+      confirmPassword
+    }
+
+    console.log(user)
   }
 
   return (
@@ -19,19 +32,27 @@ const Register = () => {
       <form onSubmit={handleSubmit}>
         <input 
           type="text"
-          placeholder="Nome" 
+          placeholder="Nome"
+          onChange={e => setName(e.target.value)}
+          value={name || ""} 
         />
         <input 
           type="email"
           placeholder="E-mail"
+          onChange={e => setEmail(e.target.value)}
+          value={email || ""} 
         />
         <input 
           type="password"
-          placeholder="Senha" 
+          placeholder="Senha"
+          onChange={e => setPassword(e.target.value)}
+          value={password || ""}  
         />
         <input 
           type="password"
-          placeholder="Confirme a senha" 
+          placeholder="Confirme a senha"
+          onChange={e => setConfirmPassword(e.target.value)}
+          value={confirmPassword || ""}  
         />
         <input type="submit" value="Cadastrar"/>
       </form>
