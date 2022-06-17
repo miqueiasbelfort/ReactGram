b88c57101c176ca615868d6a84e878170bf57b87
miqueiasbelfort <kawasousa1554@gmail.com>
2022-06-17 09:42:30 -0300

    authentication user

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,9 @@ import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom"
 
 import './App.css'
 
+//Hooks
+import { useAuth } from './hooks/useAuth'
+
 //components
 import Navbar from './components/Navbar'
 import Footer from "./components/Footer"
@@ -13,6 +16,11 @@ import Login from './pages/Auth/Login'
 import Register from './pages/Auth/Register'
 
 function App() {
+  const {auth, loading} = useAuth()
+
+  if(loading){
+    return <p>Carregando...</p>
+  }
 
   return (
     <div className="App">
@@ -22,9 +30,9 @@ function App() {
         <div className='container'>
           <Routes>
 
-          <Route path="/" element={<Home/>}/>
-          <Route path="/login" element={<Login/>}/>
-          <Route path="/register" element={<Register/>}/>
+          <Route path="/" element={auth ? <Home/> : <Navigate to="/login"/>}/>
+          <Route path="/login" element={!auth ? <Login/> : <Navigate to="/"/>}/>
+          <Route path="/register" element={!auth ? <Register/> : <Navigate to="/" />}/>
 
           </Routes>
         </div>
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
-import "./Navbar.css"
+import "./Navbar.css";
 
 //components
-import {NavLink, Link} from "react-router-dom"
+import { NavLink, Link } from "react-router-dom";
 import {
   BsSearch,
   BsHouseDoorFill,
@@ -9,38 +9,62 @@ import {
   BsFillCameraFill,
 } from "react-icons/bs";
 
+// Hooks
+import { useState } from "react";
+import { useAuth } from "../hooks/useAuth";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
 const Navbar = () => {
+  const { auth } = useAuth();
+  const { user } = useSelector((state) => state.auth);
+
   return (
     <nav id="nav">
-        <Link to="/">ReactGram</Link>
-        
-        <form id="search-form">
-          <BsSearch/>
-          <input 
-            type="text"
-            placeholder="Pesquisar" 
-          />
-        </form>
+      <Link to="/">ReactGram</Link>
 
-        <ul id="nav-links">
+      <form id="search-form">
+        <BsSearch />
+        <input type="text" placeholder="Pesquisar" />
+      </form>
+
+      <ul id="nav-links">
+        {auth ? (
+          <>
             <li>
               <NavLink to="/">
-                <BsHouseDoorFill/>
+                <BsHouseDoorFill />
+              </NavLink>
+            </li>
+            {user && (
+              <li>
+                <NavLink to={`/users/${user._id}`}>
+                  <BsFillCameraFill />
+                </NavLink>
+              </li>
+            )}
+            <li>
+              <NavLink to="/profile">
+                <BsFillPersonFill/>
               </NavLink>
             </li>
             <li>
-              <NavLink to="/login">
-                Entrar
-              </NavLink>
+              <span>Sair</span>
+            </li>
+          </>
+        ) : (
+          <>
+            <li>
+              <NavLink to="/login">Entrar</NavLink>
             </li>
             <li>
-              <NavLink to="/register">
-                Cadastrar
-              </NavLink>
+              <NavLink to="/register">Cadastrar</NavLink>
             </li>
-        </ul>
+          </>
+        )}
+      </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
