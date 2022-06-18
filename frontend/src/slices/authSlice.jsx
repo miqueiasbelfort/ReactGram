507741fd5d7c6099518a6feb2e99d4303a88e928
miqueiasbelfort <kawasousa1554@gmail.com>
2022-06-18 11:01:48 -0300

    login

--- frontend/src/slices/authSlice.jsx
+++ frontend/src/slices/authSlice.jsx
@@ -10,7 +10,7 @@ const initialState = {
     loading: false
 }
 
-// REgister an user and sign in
+// Register an user and sign in
 export const register = createAsyncThunk("auth/register", async (user, thunkAPI) => {
 
     const data = await authService.register(user)
@@ -27,6 +27,16 @@ export const register = createAsyncThunk("auth/register", async (user, thunkAPI)
 //Logut an user
 export const logout = createAsyncThunk("auth/logout", async () => {
     await authService.logout()
+})
+
+// Sign in an user
+export const login = createAsyncThunk("auth/login", async (user, thunkAPI) => {
+    const data = await authService.login(user)
+    // check for errors
+    if(data.errors){
+        return thunkAPI.rejectWithValue(data.errors[0])
+    }
+    return data;
 })
 
 export const authSlice = createSlice({
@@ -60,6 +70,21 @@ export const authSlice = createSlice({
                 state.loading = false,
                 state.success = true,
                 state.error = null,
+                state.user = null
+            })
+            .addCase(login.pending, (state) => {
+                state.loading = true,
+                state.error = false
+            })
+            .addCase(login.fulfilled, (state, action) => {
+                state.loading = false,
+                state.success = true,
+                state.error = null,
+                state.user = action.payload
+            })
+            .addCase(login.rejected, (state, action) => {
+                state.loading = false,
+                state.error = action.payload,
                 state.user = null
             })
     }
