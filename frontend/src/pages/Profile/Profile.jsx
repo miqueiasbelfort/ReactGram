0b6d90205fab76b303eddd2be23330ab12e8fa97
miqueiasbelfort <kawasousa1554@gmail.com>
2022-06-23 09:40:02 -0300

    publish a photo

--- frontend/src/pages/Profile/Profile.jsx
+++ frontend/src/pages/Profile/Profile.jsx
@@ -13,7 +13,8 @@ import {useSelector, useDispatch} from "react-redux"
 import { useParams } from "react-router-dom"
 
 // Redux
-import { getUserDetails } from "../../slices/userSlice"
+import { getUserDetails, resetMessage } from "../../slices/userSlice"
+import { publishPhoto, resetMessagePhoto } from "../../slices/photoSlice"
 
 const Profile = () => {
     const {id} = useParams()
@@ -21,14 +22,48 @@ const Profile = () => {
     const dispatch = useDispatch()
     const {user, loading} = useSelector(state => state.user)
     const {user: userAuth} = useSelector(state => state.auth)
+    const {photos, loading: loadingPhoto, message: messagePhoto, error: errorPhoto} = useSelector(state => state.photo)
 
-    // photo
+    const [title, setTitle] = useState("")
+    const [image, setImage] = useState("")
 
+    //New form and edit form refs
+    const newPhotoForm = useRef()
+    const editPhotoForm = useRef()
 
     // Load user data
     useEffect(() => {
         dispatch(getUserDetails(id))
     }, [dispatch, id])
+
+    const handleFile = (e) => {
+      // image preview
+      const image = e.target.files[0]
+      setImage(image) //update image state
+    }
+
+    const submitHandle = (e) => {
+      e.preventDefault()
+
+      const photoData = {
+        title,
+        image
+      }
+
+      // build form data
+      const formData = new FormData()
+      const photoFormData = Object.keys(photoData).forEach(key => {
+        formData.append(key, photoData[key])
+      })
+      formData.append("photo", photoFormData)
+
+      dispatch(publishPhoto(formData))
+      setTitle("")
+      
+      setTimeout(() => {
+        dispatch(resetMessagePhoto())
+      }, 2000)
+    }
 
     if(loading) {
         return <p>Caregando...</p>
@@ -45,6 +80,32 @@ const Profile = () => {
             <p>{user.bio}</p>
         </div>
       </div>
+      {id === userAuth._id && (
+        <>
+          <div className="new-photo" ref={newPhotoForm}>
+            <h3>Compartilhe seus momentos:</h3>
+            <form onSubmit={submitHandle}>
+              <label>
+                <span>Título para a foto:</span>
+                <input 
+                  type="text" 
+                  placeholder="Insira um titulo"
+                  onChange={e => setTitle(e.target.value)}
+                  value={title || ""}
+                />
+              </label>
+              <label>
+                <span>Imagem:</span>
+                <input type="file" onChange={handleFile}/>
+              </label>
+              {!loadingPhoto && <input type="submit" value="Postar"/>}
+              {loadingPhoto && <input type="submit" value="Aguarde..." disabled/>}
+            </form>
+          </div>
+          {errorPhoto && <Message msg={errorPhoto} type="error"/>}
+          {messagePhoto && <Message msg={messagePhoto} type="success"/>}
+        </>
+      )}
     </div>
   )
 }
---
+++ frontend/src/slices/photoSlice.jsx
@@ -0,0 +1,59 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import photoService from "../services/photoService";
+
+const initialState = {
+  photos: [],
+  photo: {},
+  error: false,
+  success: false,
+  loading: false,
+  message: null,
+};
+
+// funcões
+export const publishPhoto = createAsyncThunk(
+  "photo/publish",
+  async (photo, thunkAPI) => {
+    const token = thunkAPI.getState().auth.user.token;
+    const data = await photoService.publishPhoto(photo, token);
+
+    // Check for errors
+    if (data.errors) {
+      return thunkAPI.rejectWithValue(data.errors[0]);
+    }
+
+    return data;
+  }
+);
+
+export const photoSlice = createSlice({
+  name: "photo",
+  initialState,
+  reducers: {
+    resetMessagePhoto: (state) => {
+      state.message = null;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(publishPhoto.pending, (state) => {
+        (state.loading = true), (state.error = false);
+      })
+      .addCase(publishPhoto.fulfilled, (state, action) => {
+          (state.loading = false),
+          (state.success = true),
+          (state.error = null),
+          (state.photo = action.payload),
+          state.photos.unshift(state.photo),
+          (state.message = "Postado com sucesso!");
+      })
+      .addCase(publishPhoto.rejected, (state, action) => {
+          (state.loading = false),
+          (state.error = action.payload),
+          (state.photo = {});
+      });
+  },
+});
+
+export const { resetMessagePhoto } = photoSlice.actions;
+export default photoSlice.reducer;
